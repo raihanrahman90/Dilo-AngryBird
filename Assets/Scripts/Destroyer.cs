52ae42f7a15a4c63ce18bee24707915f1066b47d
Muhammad Raihan Rahman <11181061@student.itk.ac.id>
2021-10-16 23:38:26 +0800

    initiat

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        string tag = collision.gameObject.tag;
+        if(tag=="Bird" || tag=="Enemy" || tag == "Obstacle")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/OwlBird.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    [SerializeField]
+    public float power = 50f;
+    public float radius = 10f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Obstacle")
+        {
+            SetState(BirdState.HitSomething);
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, radius);
+        foreach(Collider2D benda in objects){
+            if (benda.GetComponent<Rigidbody2D>()== null)
+            {
+                continue;
+            }
+            else
+            {
+                Vector2 direction = benda.transform.position - transform.position;
+                benda.GetComponent<Rigidbody2D>().AddForce(direction * power);
+            }
+        }
+    }
+}
